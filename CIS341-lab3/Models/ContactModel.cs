811737bfdb99135a22f68dcff4a23ef442e33d66
wengert2 <joeywengert@gmail.com>
2022-09-29 19:18:43 -0500

    Add project files.

---
+++ CIS341-lab3/Models/ContactModel.cs
@@ -0,0 +1,9 @@
+﻿namespace CIS341_lab3.Models
+{
+    public class ContactModel
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Message { get; set; } = string.Empty;
+    }
+}
---
+++ CIS341-lab3/Pages/Contact.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CIS341_lab3.Pages
+{
+    public class ContactModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
