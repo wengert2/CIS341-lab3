0809b8f6c2cc83b7eb75478f701a53f42b0c74f0
wengert2 <joeywengert@gmail.com>
2022-09-29 19:31:29 -0500

    Added contact form

--- CIS341-lab3/Pages/Contact.cshtml.cs
+++ CIS341-lab3/Pages/Contact.cshtml.cs
@@ -5,6 +5,18 @@ namespace CIS341_lab3.Pages
 {
     public class ContactModel : PageModel
     {
+        [BindProperty]
+        public ContactModel? Contact { get; set; }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            return RedirectToPage("./Index");
+        }
         public void OnGet()
         {
         }
